e5ac79877a23f0714f77b1e22938e5f29b16b321
Smarth2002 <smarthkumar789@gmail.com>
2023-09-30 14:29:20 +0530

    Added and removed some files

---
+++ src/Components/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+
+function App() {
+    const [items, setItems] = useState([]);
+
+    function addItem(item) {
+        setItems((prevItems) => {
+            return [...prevItems, item];
+        });
+    }
+
+    function deleteItem(id) {
+        setItems((prevItems) => {
+            return prevItems.filter((item, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+
+            <InputArea onSubmit={addItem} />
+
+            <div>
+                <ul>
+                    {items.map((todoItem, index) => (
+                        <ToDoItem
+                            key={index}
+                            id={index}
+                            text={todoItem}
+                            onChecked={deleteItem}
+                            // ALTERNATIVE
+                            // onChecked={() => {
+                            //   setItems(
+                            //     items.filter((item) => {
+                            //       return item !== todoItem;
+                            //     })
+                            //   );
+                            // }}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
